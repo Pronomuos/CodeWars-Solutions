080f9325781e68f3022a0a616f0b0da4a2e70130
Pronomuos <71034403+Pronomuos@users.noreply.github.com>
2020-09-21 02:27:43 +0300

    Create bouncing_balls.cpp

---
+++ Bouncing_balls/bouncing_balls.cpp
@@ -0,0 +1,21 @@
+using namespace std;
+class Bouncingball
+{
+public:
+    static int bouncingBall(double h, double bounce, double window);
+};
+
+int Bouncingball::bouncingBall (double h, double bounce, double window)
+{
+  if (h<=0 || bounce<=0 || bounce>=1 || window>=h) return -1;
+  
+  int count=1;
+  h*=bounce;
+  
+  while (h>window)
+  {
+  h*=bounce; count+=2;
+  }
+  
+  return count;
+}
